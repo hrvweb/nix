448564d3718ac5ffd741be50a779cdda7c9158e3
Hrv Clan <197396286+hrvweb@users.noreply.github.com>
2026-06-05 11:33:50 +0700

    Create dev.nix

---
+++ .idx/dev.nix
@@ -0,0 +1,15 @@
+{ pkgs, ... }: {
+  channel = "stable-24.11";
+
+  packages = [
+    pkgs.qemu
+    pkgs.htop
+    pkgs.cloudflared
+    pkgs.coreutils
+    pkgs.gnugrep
+    pkgs.wget
+    pkgs.git
+    pkgs.python3
+    pkgs.cloud-image-utils
+  ];
+}
